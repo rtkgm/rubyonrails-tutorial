4e5782008afa446c4d7ab6033dab4c4fb96c66ee
RieTakegami <kurorie@gmail.com>
2026-03-12 00:09:10 +0900

    カレントclass追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def nav_link_to(name, path, **options)
+    classes = ["nav-link", options[:class]].compact
+    classes << "nav-link--current" if current_page?(path)
+
+    link_to name, path, options.merge(class: classes.join(" "))
+  end
 end
