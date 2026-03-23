2b90b3344496a60d0ae5f4dcd147b5c5f1bc9c65
RieTakegami <kurorie@gmail.com>
2026-03-23 19:06:39 +0900

    エラー修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def nav_link_to(name, path, **options)
-    classes = ["nav-link", options[:class]].compact
+    classes = [ "nav-link", options[:class] ].compact
     classes << "nav-link--current" if current_page?(path)
 
     link_to name, path, options.merge(class: classes.join(" "))
